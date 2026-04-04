0e805dfb719dca749ccc8b747c810a3cd41fd8de
qimpan <quimey.munoz@hotmail.se>
2026-04-04 12:21:12 +0200

    Add files via upload

---
+++ ovning1/src/main/java/se/su/ovning1/Book.java
@@ -0,0 +1,30 @@
+package se.su.ovning1;
+
+public class Book extends Item implements PriceableWithVAT6 {
+
+    private double price;
+    private boolean bound;
+    private String author;
+
+    public Book(String name, String author, boolean bound, double price) {
+        super(name);
+        this.author = author;
+        this.price = price;
+        this.bound = bound;
+    }
+
+    @Override
+    public double getPrice() {
+        if (bound) {
+            return price * 1.3;
+        }
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book { name='" + getName() + "', author='" + author + "', bound=" + bound +
+                ", price=" + getPrice() +
+                ", price+VAT=" + getPriceWithVAT() + " }";
+    }
+}
